8b6230c6c42944d5e421abda263ebce98d5240fa
AyazhanNurakhun <ayajan.kairatovna.2003@gmail.com>
2022-03-13 22:39:44 +0600

    first commit

--- Lab4/src/app/product-list/product-list.component.ts
+++ Lab4/src/app/product-list/product-list.component.ts
@@ -7,7 +7,7 @@ import { products } from '../products';
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent {
+export class ProductListComponent { 
 
   products = products;
 
